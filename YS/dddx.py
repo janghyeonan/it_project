299c7c3f376e5836132d9596cf121c95feaafa37
Yunsang Hwang <track1ww@naver.com>
2018-04-05 10:39:35 +0900

    Update dddx.py

--- YS/dddx.py
+++ YS/dddx.py
@@ -6,23 +6,20 @@ Created on Thu Apr  5 00:20:29 2018
 """
 #conda install -c conda-forge opencv
 import cv2
-import numpy as np  
+import numpy as np
+def hateerror():
+    img_file = 'mal.jpg'
+    img = cv2.imread(img_file,cv2.IMREAD_GRAYSCALE)
+    cv2.imwrite('mal1.jpg',img)
+    cv2.imshow('title',img)
+    cv2.waitKey(0) 
+    cv2.destroyAllWindow()    
  
-  
-img_color = cv2.imread( 'mvposter.jpg', cv2.IMREAD_COLOR )  
-  
-img_gray = cv2.cvtColor( img_color, cv2.COLOR_BGR2GRAY )  
-  
-cv2.imshow( 'color image', img_color )  
-cv2.imshow( 'gray image', img_gray )  
-  
-cv2.imwrite('result.jpg', img_gray )  
-  
-cv2.waitKey(0)  
-cv2.destroyAllWindow()  
+hateerror()
 
-blur = cv2.GaussianBlur(img_gray,(3,3),0)
-cv2.imwrite('blur.jpg',blur)
-canny=cv2.Canny(blur,100,200)
-cv2.imwrite('canny.jpg',canny)
-cnts,contours,hierarchy  = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+blur = cv2.GaussianBlur(mal1,(3,3),0)
+    cv2.imwrite('blur.jpg',blur)
+    canny=cv2.Canny(blur,100,200)
+    cv2.imwrite('canny.jpg',canny)
+    cnts,contours,hierarchy  = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
